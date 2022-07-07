a61d4019477fad1db2b7094c3f9b2f102c7d3822
Samantha Morales <smorales40@cnm.edu>
2022-07-06 18:10:38 -0600

    halfway thru

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+    initColorPicker();
+};
+
+function initColorPicker() {
+    let colorBox = document.getElementById("color-box");
+    let rgb = {
+        red: document.getElementById("red"),
+        green: document.getElementById("green"),
+        blue: document.getElementById("blue")
+    };
+}
+
+function setColorPickerEventListeners(colorBox, rgb) {
+    rgb.red.addEventListener('change',)
+}
